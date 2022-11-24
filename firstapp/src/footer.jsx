2a5fac804c6c683990f2adea69acc73f7271704f
Shubhanshu-Shukla7781 <shubhanshu.shukla@3pillarglobal.com>
2022-11-24 13:12:00 +0530

    few changes

--- firstapp/src/footer.jsx
+++ firstapp/src/footer.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 let Footer=()=>{
-    return <h1 className='foot'>Hlw everyone I am footer</h1>
+    return 
+    (<h1 className='foot'>Hlw everyone I am footer</h1>)
 }
 
 export default Footer;
